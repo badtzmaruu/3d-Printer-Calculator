09e0ea718e22cb24a888f265d0d1cd7da9024f8d
Jonathan Joubert <joubert444@protonmail.com>
2024-05-01 17:48:52 +0200

    rollback old programme, pdf working

--- 3d Printer Calculator/lib/main.dart
+++ 3d Printer Calculator/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -113,7 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
               buildPostProcessingCosts(),
               buildWasteAndFailedPrintCosts(),
               buildTaxPercentageField(),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               buildCalculateButton(),
             ],
           ),
